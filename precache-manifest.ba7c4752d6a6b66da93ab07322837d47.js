cdf4573c92928f38cd3390bd2fb6ffa53c7d5e2d
thetradecoder <thetradecoder@gmail.com>
2020-08-24 13:00:36 +0600

    Updates

---
+++ precache-manifest.ba7c4752d6a6b66da93ab07322837d47.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9d59d7566196f401902fb82dbeead46",
+    "url": "/mern-crud-app/index.html"
+  },
+  {
+    "revision": "0678413422a92b8e79bf",
+    "url": "/mern-crud-app/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "ab89917e25ab96daf856",
+    "url": "/mern-crud-app/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "0678413422a92b8e79bf",
+    "url": "/mern-crud-app/static/js/2.10226246.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/mern-crud-app/static/js/2.10226246.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab89917e25ab96daf856",
+    "url": "/mern-crud-app/static/js/main.737671d4.chunk.js"
+  },
+  {
+    "revision": "b99e9e87fe62fe7c59c0",
+    "url": "/mern-crud-app/static/js/runtime-main.41823505.js"
+  }
+]);
