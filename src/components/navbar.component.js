fe391f0ec686c2647ff8f236c4953d7903f5ef84
thetradecoder <thetradecoder@gmail.com>
2020-08-24 15:33:34 +0600

    local update

--- src/components/navbar.component.js
+++ src/components/navbar.component.js
@@ -6,7 +6,7 @@ export default function Navbar(){
 
         return(
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-            <Link to="/mern-crud-app/" className="navbar-brand">TRADECODER</Link>
+            <Link to="/mern-crud-app/" className="navbar-brand">tradecoder</Link>
             <ul className="navbar-nav ml-auto">
                 <li className="nav-item"><Link to="/mern-crud-app/" className="nav-link">Todo List</Link></li>
                 <li className="nav-item"><Link to="/mern-crud-app/add/" className="nav-link">Add Task</Link></li>
