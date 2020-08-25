46a3bce90f9ffc5e3d50940f747c01839ec43a25
thetradecoder <thetradecoder@gmail.com>
2020-08-25 23:00:07 +0600

    local update

--- src/components/documents.component.js
+++ src/components/documents.component.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import SideNav from './side-nav.documents.components';
 
 export default function Documents(){
     return(
         <div className="container">
             <h1 className="text-center">Documents for a MERN Stack CRUD app</h1>
+            <div>
+                <SideNav />
+            </div>
 
         </div>
     );
---
+++ src/components/side-nav.documents.components.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../App.css';
+
+
+export default function SideNav(){
+    return(
+        <div className="side-nav">
+            <a herf="#" >Introduction</a>
+
+        </div>
+    );
+}
