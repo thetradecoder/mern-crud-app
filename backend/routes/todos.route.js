6374e24236d679c9dddfd34d9d6070f266869f27
thetradecoder <thetradecoder@gmail.com>
2020-08-26 23:25:24 +0600

    local update

--- backend/routes/todos.route.js
+++ backend/routes/todos.route.js
@@ -4,13 +4,7 @@ const Todo = require('../models/todos.model');
 
 router.route('/').get((req, res)=>{
     Todo.find({})
-    .then(data=>res.json(data.map(d=>{return {
-        username:d.username, 
-        heading: d.heading, 
-        description:d.description, 
-        startdate:d.startdate, 
-        deadline: d.deadline
-    }})))
+    .then(data=>res.json(data.map(d=>d)))
     .catch(err=>res.status(400).send(err));
 });
 
@@ -25,7 +19,29 @@ router.route('/add').post((req, res)=>{
     const newTodo = new Todo({username, heading, description, startdate, deadline, editpassword});
     newTodo.save()
     .then(()=>res.send('New task added'))
-    .catch(err=>res.status(400).send(err))
+    .catch(err=>res.status(400).send(err));
+});
+
+router.route('/:id').get((req,res)=>{
+    Todo.findById(req.params.id)
+    .then(data=>res.json(data))
+    .catch(err=>res.status(400).send(err));
+});
+
+router.route('/update/:id').post((req, res)=>{
+    Todo.findById(req.params.id)
+    .then(data=>{
+        data.username = req.body.username;
+        data.heading = req.body.heading;
+        data.description = req.body.description;
+        data.startdate = req.body.startdate;
+        data.deadline = req.body.deadline;
+
+        data.save()
+        .then(d=>res.send('Todo updated'))
+        .catch(err=>res.status(400).send(err))
+    })
+    .catch(err=>res.status(400).send(err));
 });
 
 
