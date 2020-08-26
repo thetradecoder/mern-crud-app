ebaf4f451a3b48445d28e0082b9f1d3217528313
thetradecoder <thetradecoder@gmail.com>
2020-08-26 13:17:19 +0600

    local update

--- src/components/navbar.component.js
+++ src/components/navbar.component.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.min.js';
+import 'bootstrap/dist/js/bootstrap';
 import 'jquery/dist/jquery.min.js';
 import 'popper.js/dist/popper.min.js';
 
