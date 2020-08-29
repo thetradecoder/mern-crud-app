dd8bdfe06e9ef429673d8003cb9e96d61233e50e
thetradecoder <thetradecoder@gmail.com>
2020-08-29 16:59:03 +0600

    local update

--- src/notes.js
+++ src/notes.js
@@ -12,3 +12,13 @@ import {useEffect} from "react";
 // useEffect(callback, []);
 useEffect(()=>{}, []);
 
+
+
+// Dynamically generated html id attribute
+// since html id attribute can not be started
+// with number, it is very common to get trouble 
+// while generating id automatically from 
+// a data source, it will fail if the source
+// has the data starting with a number
+// To solve this issue we can add a common letter
+// before the source data 
